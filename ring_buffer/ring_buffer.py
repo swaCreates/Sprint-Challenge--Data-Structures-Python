c0cae2a20a2e82f378405d81b55d069406571d68
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-06-26 21:59:25 -0400

    working on ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = int(capacity)
+        self.inventory = []
 
     def append(self, item):
-        pass
+                
+        while len(self.inventory) <= self.capacity:
+            if len(self.inventory) <= self.capacity:
+                self.inventory.append(item)
+            elif len(self.inventory) > self.capacity:
+                # self.inventory.pop(0)
+                # self.inventory.insert(0, item)
 
     def get(self):
-        pass
+        new_list = []
+        for el in self.inventory:
+            if el is not None:
+                new_list.append(el)
